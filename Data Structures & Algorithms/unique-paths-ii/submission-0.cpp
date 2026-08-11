b8ce5090b1d5b05d727f589adb1fff14f59ca076
anirudh0510 <darkstarchampion05@gmail.com>
2026-08-11 10:41:06 +0530

    Add: unique-paths-ii - submission-0

---
+++ Data Structures & Algorithms/unique-paths-ii/submission-0.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int solve(int i, int j, vector<vector<int>>& obstacleGrid , vector<vector<int>>&dp) {
+
+        if(i < 0 || j < 0)
+            return 0;
+
+        // Check obstacle first
+        if(obstacleGrid[i][j] == 1)
+            return 0;
+
+        // Starting cell
+        if(i == 0 && j == 0)
+            return dp[i][j]= 1;
+
+        if(dp[i][j] != -1)return dp[i][j];
+
+        int up = solve(i - 1, j, obstacleGrid ,dp);
+        int left = solve(i, j - 1, obstacleGrid ,dp);
+
+        return dp[i][j]= up + left;
+    }
+
+    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+        int n = obstacleGrid.size();
+        int m = obstacleGrid[0].size();
+        vector<vector<int>>dp(n , vector<int>(m , -1));
+
+        return solve(n - 1, m - 1, obstacleGrid ,dp);
+    }
+};
